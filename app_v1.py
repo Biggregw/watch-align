a19905595e7124c401426a33a699e4f3663b281e
Greg W <gregwhitehead71@gmail.com>
2026-09-07 13:20:12 +0100

    Open V1 interface by default in Windows launcher

--- app_v1.py
+++ app_v1.py
@@ -10,6 +10,7 @@ from v1_reference_library import install_reference_library
 install(legacy_launcher.backend)
 install_full(legacy_launcher.backend, perspective_diagnostics)
 install_reference_library(legacy_launcher.backend, v1_full)
+legacy_launcher.URL = f"http://127.0.0.1:{legacy_launcher.PORT}/v1"
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
