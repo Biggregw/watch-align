49926ac346a3c9e452b34778202d677edae9d280
Greg W <gregwhitehead71@gmail.com>
2026-09-07 20:03:21 +0100

    Install V1.2.1 bugfix layer in server app

--- main_v1.py
+++ main_v1.py
@@ -10,6 +10,7 @@ import v1_official_sources
 from v1_official_sources import install as install_official_sources
 from v1_ux_v120 import install as install_v120
 from v1_ux_v120_patch import install as install_v120_patch
+from v1_bugfix_v121 import install as install_v121
 
 install(backend)
 install_confidence_v110(v1_full)
@@ -18,4 +19,5 @@ install_reference_library(backend, v1_full)
 install_official_sources(backend, v1_full, v1_reference_library)
 install_v120(backend, v1_full, v1_reference_library, v1_official_sources, perspective_diagnostics)
 install_v120_patch(backend)
+install_v121(backend, v1_full, v1_official_sources)
 app = backend.app
