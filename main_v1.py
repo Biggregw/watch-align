0365d6a2e719611eecd7b8da4b4951003a13f165
Greg W <gregwhitehead71@gmail.com>
2026-09-07 20:30:13 +0100

    Wire V1.2.2 runtime fixes

--- main_v1.py
+++ main_v1.py
@@ -8,9 +8,11 @@ import v1_reference_library
 from v1_reference_library import install_reference_library
 import v1_official_sources
 from v1_official_sources import install as install_official_sources
+import v1_ux_v120
 from v1_ux_v120 import install as install_v120
 from v1_ux_v120_patch import install as install_v120_patch
 from v1_bugfix_v121 import install as install_v121
+from v1_bugfix_v122 import install as install_v122
 
 install(backend)
 install_confidence_v110(v1_full)
@@ -20,4 +22,5 @@ install_official_sources(backend, v1_full, v1_reference_library)
 install_v120(backend, v1_full, v1_reference_library, v1_official_sources, perspective_diagnostics)
 install_v120_patch(backend)
 install_v121(backend, v1_full, v1_official_sources)
+install_v122(backend, v1_full, v1_ux_v120, v1_official_sources, perspective_diagnostics)
 app = backend.app
