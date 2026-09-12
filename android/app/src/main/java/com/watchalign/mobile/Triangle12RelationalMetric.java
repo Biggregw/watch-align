cbf1d1cc38be4c652d19ac677004c2da91440f12
Greg W <gregwhitehead71@gmail.com>
2026-09-12 18:29:23 +0100

    Make triangle relation math JVM-testable

--- android/app/src/main/java/com/watchalign/mobile/Triangle12RelationalMetric.java
+++ android/app/src/main/java/com/watchalign/mobile/Triangle12RelationalMetric.java
@@ -11,18 +11,21 @@ public final class Triangle12RelationalMetric {
     private Triangle12RelationalMetric() {}
 
     public static Result measure(PointF left,PointF right,PointF apex,PointF minute60Inner,PointF crownTop,PointF centre,PointF p12){
-        PointF baseMid=new PointF((left.x+right.x)/2f,(left.y+right.y)/2f);
-        float ux=p12.x-centre.x,uy=p12.y-centre.y,un=(float)Math.hypot(ux,uy);if(un<1e-6f)un=1;ux/=un;uy/=un;
+        return measureRaw(left.x,left.y,right.x,right.y,apex.x,apex.y,minute60Inner.x,minute60Inner.y,crownTop.x,crownTop.y,centre.x,centre.y,p12.x,p12.y);
+    }
+
+    static Result measureRaw(float lx,float ly,float rx,float ry,float ax,float ay,float mx,float my,float cx,float cy,float ox,float oy,float p12x,float p12y){
+        float baseX=(lx+rx)/2f,baseY=(ly+ry)/2f;
+        float ux=p12x-ox,uy=p12y-oy,un=(float)Math.hypot(ux,uy);if(un<1e-6f)un=1;ux/=un;uy/=un;
         float tx=-uy,ty=ux;
-        float bw=dist(left,right);if(bw<1e-6f)bw=1e-6f;
-        float baseGap=((minute60Inner.x-baseMid.x)*ux+(minute60Inner.y-baseMid.y)*uy)/bw;
-        float apexGap=((apex.x-crownTop.x)*ux+(apex.y-crownTop.y)*uy)/bw;
-        float height=((apex.x-baseMid.x)*ux+(apex.y-baseMid.y)*uy)/bw;
-        float baseDx=right.x-left.x,baseDy=right.y-left.y;
+        float bw=(float)Math.hypot(rx-lx,ry-ly);if(bw<1e-6f)bw=1e-6f;
+        float baseGap=((mx-baseX)*ux+(my-baseY)*uy)/bw;
+        float apexGap=((ax-cx)*ux+(ay-cy)*uy)/bw;
+        float height=((ax-baseX)*ux+(ay-baseY)*uy)/bw;
+        float baseDx=rx-lx,baseDy=ry-ly;
         float baseAlongT=baseDx*tx+baseDy*ty,baseAlongU=baseDx*ux+baseDy*uy;
         float rot=(float)Math.toDegrees(Math.atan2(baseAlongU,baseAlongT));
-        float lateral=(baseMid.x-centre.x)*tx+(baseMid.y-centre.y)*ty;
+        float lateral=(baseX-ox)*tx+(baseY-oy)*ty;
         return new Result(baseGap,apexGap,height,rot,lateral);
     }
-    private static float dist(PointF a,PointF b){return (float)Math.hypot(a.x-b.x,a.y-b.y);}
 }
